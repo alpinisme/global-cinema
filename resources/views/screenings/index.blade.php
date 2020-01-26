c3b3615ae44db3777d67f22015147ebd203e13f2
Matt Nelson <menelso2@illinois.edu>
2020-01-26 14:06:54 -0600

    format

--- resources/views/screenings/index.blade.php
+++ resources/views/screenings/index.blade.php
@@ -5,6 +5,7 @@
   <h1 class="title">Screenings</h1>
 
   <table>
+
     <thead>
       <th>date</th>
       <th>theater</th>
@@ -12,20 +13,22 @@
       <th>created by</th>
       <th>delete</th>
     </thead>
+
     @foreach ($screenings as $screening)
-        <tr>
-          <td><a href="/screenings/{{$screening->id}}">{{$screening->date}}</a></td>
-          <td>{{$screening->theater->name}}</td>
-          <td>{{$screening->film->title}}</td>
-          <td>{{$screening->createdBy/*->name*/}}</td>
-          <td>
-            <form method="POST" action="/screenings/{{$screening->id}}">
-              @csrf @method('DELETE')
-              <input type="submit" value="delete" class="button danger tertiary-action">
-            </form>
-          </td>
-        </tr>
+    <tr>
+      <td><a href="/screenings/{{$screening->id}}">{{$screening->date}}</a></td>
+      <td>{{$screening->theater->name}}</td>
+      <td>{{$screening->film->title}}</td>
+      <td>{{$screening->createdBy->name}}</td>
+      <td>
+        <form method="POST" action="/screenings/{{$screening->id}}">
+          @csrf @method('DELETE')
+          <input type="submit" value="delete" class="button danger tertiary-action">
+        </form>
+      </td>
+    </tr>
     @endforeach
+
   </table>
 
   <a href="/home" class="button secondary-action">Back Home</a>
