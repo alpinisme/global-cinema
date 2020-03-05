5164f5eefa9c691b114497ce237ee63d3f8f974b
Matt Nelson <menelso2@illinois.edu>
2020-03-04 19:27:49 -0600

    fix assignment factory

--- database/factories/AssignmentFactory.php
+++ database/factories/AssignmentFactory.php
@@ -7,6 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Assignment::class, function (Faker $faker) {
     return [
-        'Assignment' => $faker->sentence()
+        'Assignment' => $faker->sentence(),
+        'student_id' => factory('App\User')->create(),
+        'instructor_id' => factory('App\User')->create()
     ];
 });
