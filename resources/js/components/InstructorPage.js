d04981a65c1c0f1d7da3e9910f52d28d20e49812
Matthew Nelson <matthewnelson@Matthews-MBP.localdomain>
2020-06-03 18:09:35 -0500

    update instructor page

--- resources/js/components/InstructorPage.js
+++ resources/js/components/InstructorPage.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const InstructorPage = ({ setErrors }) => {
     const [studentData, setStudentData] = useState(null);
+    const [selectedStudent, setSelectedStudent] = useState(null);
 
     /**
      * get student data from api
@@ -22,33 +23,60 @@ const InstructorPage = ({ setErrors }) => {
         padding: '0.5rem'
     };
 
+    const margin = {
+        marginTop: '2rem'
+    };
+
     return studentData ? (
-        <table style={{ marginTop: '2rem' }}>
-            <thead style={titleStyle}>
-                <tr>
-                    <td colSpan="3" style={{ textAlign: 'center' }}>
-                        Number of Days Your Students Completed
-                    </td>
-                </tr>
-                <tr>
-                    <td>name</td>
-                    <td>email</td>
-                    <td>days completed</td>
-                </tr>
-            </thead>
-            <tbody>
-                {studentData.map(student => (
-                    <tr key={student.info.id}>
-                        <td>{student.info.name}</td>
-                        <td>{student.info.email}</td>
-                        <td>{student.datesCompleted.length}</td>
+        <>
+            <h2 style={margin}>Instructor Overview</h2>
+            <p>
+                click on specific rows for a full list of days completed by a
+                student (at page bottom)
+            </p>
+            <table style={margin}>
+                <thead style={titleStyle}>
+                    <tr>
+                        <td colSpan="3" style={{ textAlign: 'center' }}>
+                            Number of Days Your Students Completed
+                        </td>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                    <tr>
+                        <td>name</td>
+                        <td>email</td>
+                        <td>days completed</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {studentData.map(student => (
+                        <tr
+                            key={student.info.id}
+                            onClick={() => setSelectedStudent(student)}
+                        >
+                            <td>{student.info.name}</td>
+                            <td>{student.info.email}</td>
+                            <td>{student.datesCompleted.length}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            {selectedStudent && (
+                <div className="box">
+                    <ul>
+                        {selectedStudent.datesCompleted.map(date => (
+                            <li key={date}>{date}</li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </>
     ) : (
         '...loading'
     );
+};
+InstructorPage.propTypes = {
+    setErrors: PropTypes.func.isRequired
 };
 
 export default InstructorPage;
