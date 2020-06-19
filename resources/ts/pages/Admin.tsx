0710b2fe0f3fc2672a58370e7933756682aa6f4a
Matthew Nelson <menelso2@illinois.edu>
2020-06-18 21:47:53 -0500

    progress on Admin page

--- resources/ts/pages/Admin.tsx
+++ resources/ts/pages/Admin.tsx
@@ -3,17 +3,42 @@ import type { Film, Theater, User } from '../types/api';
 import Month from '../components/Month';
 import MonthPicker from '../components/MonthPicker';
 import Autosuggest from '../components/Autosuggest';
-import { usePostRequest } from '../utils/hooks';
 import Clipboard from '../components/Clipboard';
-import styles from './Admin.scss';
 import Collapsible from '../components/Collapsible';
+import { usePostRequest } from '../utils/hooks';
+import styles from './Admin.scss';
 
 type Action = 'edit users' | 'password reset' | 'enter screenings';
 
+/**
+ * Given a `setState` function and an `Action`, this curried function
+ * compares the new `Action` against the previous one, and
+ * sets the state to null if they are the same (re-click)
+ * or to the new `Action` if not
+ *
+ * @param fn react setState function
+ * @param next action that is clicked on
+ */
+const handleClick = (fn: Dispatch<SetStateAction<Action | null>>, next: Action) => {
+    const compare = (prev: Action | null) => {
+        if (prev == next) {
+            return null;
+        }
+        return next;
+    };
+    fn(compare);
+};
+
+/**
+ * Homepage for admin users, where they can alter user privileges,
+ * manually reset passwords, and input data for any city and date they like.
+ */
 const AdminPage = ({ useGetRequest }: Props): ReactElement => {
     const [month, setMonth] = useState<string | null>(null);
     const [action, setAction] = useState<Action | null>(null);
-    const [result, makePostRequest] = usePostRequest<string, string>();
+    const [user, setUser] = useState<User | null>(null);
+
+    const [users] = useGetRequest<User[]>('/users', e => `Users could not be loaded: ${e}`);
 
     const [films, setFilms] = useGetRequest<Film[]>(
         '/films',
@@ -25,10 +50,8 @@ const AdminPage = ({ useGetRequest }: Props): ReactElement => {
         e => `Theaters could not be loaded: ${e}`
     );
 
-    const [users] = useGetRequest<User[]>(
-        '/users',
-        e => `Users could not be loaded: ${e}`
-    );
+    const [passResetResult, makePassResetRequest] = usePostRequest<string, string>();
+    const [alterPrivilegesResult, makeAlterPrivilegesRequest] = usePostRequest<User, string>();
 
     /**
      * add film to list of films in state;
@@ -36,43 +59,26 @@ const AdminPage = ({ useGetRequest }: Props): ReactElement => {
      * (such a state should be impossible, though)
      * @param film Film
      */
-    const addFilm = (film: Film) =>
-        setFilms(old => (old ? [film, ...old] : [film]));
+    const addFilm = (film: Film) => setFilms(old => (old ? [film, ...old] : [film]));
 
     /**
-     * Given a `setState` function and an `Action`, this curried function
-     * compares the new `Action` against the previous one, and
-     * sets the state to null if they are the same (re-click)
-     * or to the new `Action` if not
-     *
-     * @param fn react setState function
-     * @param next action that is clicked on
+     * checks if the clicked on collapsible is already open
+     * @param el Action assigned to the collapsible element
      */
-    const handleClick = (
-        fn: Dispatch<SetStateAction<Action | null>>,
-        next: Action
-    ) => {
-        const compare = (prev: Action | null) => {
-            if (prev == next) {
-                return null;
-            }
-            return next;
-        };
-        fn(compare);
-    };
-
     const isOpen = (el: Action) => el == action;
 
-    const toggleActive = (content: Action) =>
-        action == content
-            ? `${styles.content} ${styles.active}`
-            : styles.content;
+    const suggestUsersConfig = {
+        label: 'user',
+        keys: ['name', 'email'],
+        displayMatch: (u: User) => `${u.name} (${u.email})`,
+        options: users ?? [],
+    };
 
     return month ? (
         <Month
             month={month}
-            films={films ?? ([] as Film[])}
-            theaters={theaters ?? ([] as Theater[])}
+            films={films ?? []}
+            theaters={theaters ?? []}
             addFilm={addFilm}
             cancel={() => setMonth(null)}
         />
@@ -87,38 +93,36 @@ const AdminPage = ({ useGetRequest }: Props): ReactElement => {
                         handleClick={() => handleClick(setAction, 'edit users')}
                         buttonLabel="edit users"
                     >
-                        <>hear you go</>
+                        {!user ? (
+                            <Autosuggest config={suggestUsersConfig} handleSubmit={console.log} />
+                        ) : (
+                            <div></div>
+                        )}
                     </Collapsible>
                 </li>
+
                 <li>
                     <Collapsible
                         open={isOpen('password reset')}
-                        handleClick={() =>
-                            handleClick(setAction, 'password reset')
-                        }
+                        handleClick={() => handleClick(setAction, 'password reset')}
                         buttonLabel="edit users"
                     >
-                        {!result.data ? (
+                        {!passResetResult.data ? (
                             <Autosuggest
-                                label={'user'}
-                                keys={['name', 'email']}
-                                options={users ?? []}
-                                displayMatch={match =>
-                                    `${match.name} (${match.email})`
-                                }
+                                config={suggestUsersConfig}
                                 handleSubmit={id =>
-                                    makePostRequest(`/password/reset/${id}`, '')
+                                    makePassResetRequest(`/password/reset/${id}`, '')
                                 }
-                                handleManualAdd={name => console.log(name)}
                             />
                         ) : (
                             <div>
                                 Reset link:
-                                <Clipboard content={result.data} />
+                                <Clipboard content={passResetResult.data} />
                             </div>
                         )}
                     </Collapsible>
                 </li>
+
                 <li>
                     <Collapsible
                         open={isOpen('enter screenings')}
