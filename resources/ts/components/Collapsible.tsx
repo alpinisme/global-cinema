2a2e854ac24a288ac5e898467bf8650f6dc576b8
Matthew Nelson <menelso2@illinois.edu>
2020-06-20 02:06:50 -0500

    bug fix (style went stale with child rerenders)

--- resources/ts/components/Collapsible.tsx
+++ resources/ts/components/Collapsible.tsx
@@ -1,23 +1,22 @@
-import React, { ReactElement, useRef } from 'react';
+import React, { ReactElement } from 'react';
 import styles from './Collapsible.scss';
 
-const Collapsible = ({ open, handleClick, children, buttonLabel }: Props): ReactElement => {
-    const contentRef = useRef<HTMLDivElement>(null);
-
+const Collapsible = ({ open, handleClick, children, label }: Props): ReactElement => {
     const contentClassName = open ? `${styles.content} ${styles.open}` : styles.content;
 
     const arrowClassName = open ? `${styles.arrow} ${styles.open}` : styles.arrow;
 
-    const maxHeight = open ? contentRef.current?.scrollHeight + 'px' : '0px';
+    const maxHeight = open ? 'none' : '0px';
+
     return (
         <>
             <div className={styles.div} onClick={handleClick}>
-                <span className={styles.label}>{buttonLabel}</span>
+                <span className={styles.label}>{label}</span>
                 <button className={styles.button}>
                     <span className={arrowClassName}></span>
                 </button>
             </div>
-            <div ref={contentRef} style={{ maxHeight }} className={contentClassName}>
+            <div style={{ maxHeight }} className={contentClassName}>
                 {children}
             </div>
         </>
@@ -29,6 +28,6 @@ export default Collapsible;
 export interface Props {
     open: boolean;
     handleClick: () => void;
-    buttonLabel: string;
+    label: string;
     children: ReactElement;
 }
