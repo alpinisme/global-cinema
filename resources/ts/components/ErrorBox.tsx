da5b2f41a0d2bfa40475d196676c38e61b0a872c
Matthew Nelson <menelso2@illinois.edu>
2020-06-20 18:50:23 -0500

    style errorbox

--- resources/ts/components/ErrorBox.tsx
+++ resources/ts/components/ErrorBox.tsx
@@ -1,9 +1,10 @@
 import React, { ReactElement } from 'react';
+import styles from './ErrorBox.scss';
 
 const ErrorBox = ({ errors }: Props): ReactElement => {
     const errList = errors.map(e => (
-        <ul key={e}>
-            <li className="err-msg">{e}</li>
+        <ul key={e} className={styles.box}>
+            <li className={styles.message}>{e}</li>
         </ul>
     ));
 
