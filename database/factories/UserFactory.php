8ef01a1a83ba4f65f0229b3009e65de862b0bb49
Matthew Nelson <menelso2@illinois.edu>
2021-01-13 20:04:11 -0600

    Add 'just registered' state to User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
-        'role' => User::DEFAULT_TYPE,
+        'role' => $faker->randomElement(User::ROLES),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
     ];
@@ -38,3 +38,9 @@ $factory->state(User::class, 'student', [
 $factory->state(User::class, 'instructor', [
     'role' => 'instructor',
 ]);
+
+$factory->state(User::class, 'just registered', function (Faker $faker) {
+    return [
+        'role' => $faker->randomElement(User::REGISTERABLE_ROLES),
+    ];
+});
