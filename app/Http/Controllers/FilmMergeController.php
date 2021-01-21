8448d75f3ee1ca54557a47f190263cf2dc6e5a88
Matthew Nelson <menelso2@illinois.edu>
2021-01-20 22:52:32 -0600

    Make Request class for FilmMergeController

--- app/Http/Controllers/FilmMergeController.php
+++ app/Http/Controllers/FilmMergeController.php
@@ -3,16 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Film;
+use App\Http\Requests\FilmMergeRequest;
 use App\Screening;
-use Illuminate\Http\Request;
 
 class FilmMergeController extends Controller
 {
-    // TODO Validate request input
-    public function update(Request $request)
+    public function update(FilmMergeRequest $request)
     {
-        $from = $request->input('from');
-        $to = $request->input('to');
+        $input = $request->validated();
+        $from = $input['from'];
+        $to = $input['to'];
 
         Screening::query()->where('film_id', $from)->update(['film_id' => $to]);
         Film::query()->where('id', $from)->delete();
