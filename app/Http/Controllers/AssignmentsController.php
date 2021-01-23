ae4740b20718b9bf3e8644508a044d62663115c6
Matthew Nelson <menelso2@illinois.edu>
2021-01-22 22:58:14 -0600

    Refactor AssignmentsController
    Again, slowly removing dependence on StandardResourceController.
    In this case, there didn't seem to be any 'real' dependence at all

--- app/Http/Controllers/AssignmentsController.php
+++ app/Http/Controllers/AssignmentsController.php
@@ -5,33 +5,8 @@ namespace App\Http\Controllers;
 use DB;
 use Illuminate\Http\Request;
 
-class AssignmentsController extends StandardResourceController
+class AssignmentsController extends Controller
 {
-    /**
-     *  Eloquent model to which the controller is providing access, e.g. 'App\Product' or Product::class
-     */
-    protected $model = 'App\Assignment';
-
-    /**
-     * All writable columns in database, given as array of `field_name => validation_rule` pairs
-     */
-    protected $fields = [];
-
-    /**
-     * the database table name, usually plural, e.g., "products"
-     */
-    protected $tableName = 'assignments';
-
-    /**
-     * how to refer to a singular instance, e.g., "product"
-     */
-    protected $objectName = 'assignment';
-
-    /**
-     * column name to order by when selecting all
-     */
-    protected $orderBy = '';
-
     public function getAssignedCity()
     {
         $query = DB::select("Select column_default from information_schema.columns where table_name='theaters' and column_name='city_id'");
