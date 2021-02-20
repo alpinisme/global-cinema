ba76abfa0c4969e2b13ea950b49dc273fb1ae073
Matthew Nelson <menelso2@illinois.edu>
2021-02-20 10:46:28 -0600

    Switch to Laravel 8-style routing

--- routes/web.php
+++ routes/web.php
@@ -11,60 +11,75 @@
 |
 */
 
+use App\Http\Controllers\ActivityReviewController;
+use App\Http\Controllers\AssignmentSettingController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\FilmController;
+use App\Http\Controllers\FilmMergeController;
+use App\Http\Controllers\GradingController;
+use App\Http\Controllers\ProgressReviewController;
+use App\Http\Controllers\ScreeningController;
+use App\Http\Controllers\ScreeningCsvUploadController;
+use App\Http\Controllers\TempFilmMergeController;
+use App\Http\Controllers\TheaterController;
+use App\Http\Controllers\TheaterCsvUploadController;
+use App\Http\Controllers\UserController;
 use App\User;
 
 Route::get('/', function () {
     return view('app');
 });
-Route::get('/test', 'TempFilmMergeController');
-Route::get('/grading', 'GradingController@index')->middleware(['can:grade']);
+Route::get('/test', TempFilmMergeController::class);
+Route::get('/grading', [GradingController::class, 'index'])->middleware(['can:grade']);
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/screenings/{date}', 'ScreeningController@date');
-    Route::get('/films/search/{string}', 'FilmController@search');
+    Route::get('/screenings/{date}', [ScreeningController::class, 'date']);
+    Route::get('/films/search/{string}', [FilmController::class, 'search']);
 
-    Route::apiResource('/screenings', 'ScreeningController');
-    Route::apiResource('/films', 'FilmController');
-    Route::apiResource('/cities', 'CityController');
-    Route::apiResource('/theaters', 'TheaterController');
+    Route::apiResource('/screenings', ScreeningController::class);
+    Route::apiResource('/films', FilmController::class);
+    Route::apiResource('/cities', CityController::class);
+    Route::apiResource('/theaters', TheaterController::class);
 
     Route::get('/assignment', function () {
-        return auth()->user()->assignment->assignment;
+        return auth()->user()->assignment->assignment; // TODO: switch to controller
     });
 
     Route::get('/user', function () {
-        return auth()->user();
+        return auth()->user(); // TODO: switch to controller
     });
 
     Route::get('/role', function () {
-        return auth()->user()->role;
+        return auth()->user()->role; // TODO: switch to controller
     });
-    Route::post('/csv/screenings', 'ScreeningCsvUploadController');
-    Route::post('/csv/theaters', 'TheaterCsvUploadController');
+    Route::post('/csv/screenings', ScreeningCsvUploadController::class);
+    Route::post('/csv/theaters', TheaterCsvUploadController::class);
 });
 
-Route::get('/map', 'ScreeningController@map');
-Route::get('/map/{city}/{date}', 'ScreeningController@geoJSON');
-Route::get('/cities', 'CityController@index');
+Route::get('/map', [ScreeningController::class, 'map']);
+Route::get('/map/{city}/{date}', [ScreeningController::class, 'geoJSON']);
+Route::get('/cities', [CityController::class, 'index']);
 Route::get('/instructors', function () {
-    return User::select('id', 'name')->where('role', 'instructor')->get();
+    return User::select('id', 'name')->where('role', 'instructor')->get(); // TODO: switch to controller
 });
 
 Route::group(['middleware' => ['can:see admin tools']], function () {
-    Route::apiResource('/users', 'UserController');
+    Route::apiResource('/users', UserController::class);
 
-    Route::get('/assigned_city', 'AssignmentSettingController@show');
-    Route::post('/assigned_city', 'AssignmentSettingController@create');
+    Route::get('/assigned-city', [AssignmentSettingController::class, 'show']);
+    Route::post('/assigned-city', [AssignmentSettingController::class, 'create']);
 
-    Route::post('/password/reset/{id}', 'Auth\ResetPasswordController@getResetLink');
+    Route::post('/password/reset/{id}', [ResetPasswordController::class, 'getResetLink']);
 
-    Route::get('/review/{category}', 'ActivityReviewController');
-    Route::patch('/merge/films', 'FilmMergeController@update');
+    Route::get('/review/{category}', ActivityReviewController::class);
+    Route::patch('/merge/films', [FilmMergeController::class, 'update']);
 });
 
 Auth::routes();
-Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/logout', [LoginController::class, 'logout']);
 
-Route::get('/completed/{city}', 'ProgressReviewController@index');
+Route::get('/completed/{city}', [ProgressReviewController::class, 'index']);
 
 // Route::get('/log',                          '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index'); // see github for how to add
