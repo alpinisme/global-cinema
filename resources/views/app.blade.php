86a8c0bba5c39aa6a33f0dcc5aa19fef91257422
Matthew Nelson <menelso2@illinois.edu>
2021-03-10 14:05:17 -0600

    Combine layout and app views
    Since we are now a SPA, there is no reason
    to separate our app view into layout and content files
    I will delete all other views in the near future
    But I will do that in a separate commit to make finding them easier

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,26 @@
-@extends('layout')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
+<meta charset="utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="author" content="Matthew Nelson">
+<meta name="description" content="{{ config('app.name') }}">
+<meta name="twitter:title" content="{{ config('app.name') }}">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>{{ config('app.name') }}</title>
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/map.css') }}" />
+<link href="https://fonts.googleapis.com/css?family=Lato:300,300b,300i" rel="stylesheet">
+<script src="/js/app.js" defer></script>
+</head>
 
-<main>
-  <noscript>Please enable JavaScript to run this application</noscript>
-  <div id="root"></div>
-  <div id="modal-root"></div>
-</main>
+<body>
+  <main>
+    <noscript>Please enable JavaScript to run this application</noscript>
+    <div id="root"></div>
+    <div id="modal-root"></div>
+  </main>
+</body>
 
-<script src="/js/app.js"></script>
-@endsection
+</html>
