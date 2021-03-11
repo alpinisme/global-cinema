58ace29af7ca7b47fa0978da7201bf9b3c081de5
Matthew Nelson <menelso2@illinois.edu>
2021-03-10 23:29:36 -0600

    Use POST not GET for logout

--- resources/ts/components/NavBar/index.tsx
+++ resources/ts/components/NavBar/index.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
@@ -6,17 +7,20 @@ import styles from './NavBar.scss';
 const NavBar = (): ReactElement => {
     const auth = useAuth();
 
+    const handleLogout = e => {
+        e.preventDefault();
+        axios.post('/logout').then(() => auth.logout());
+    };
+
     return (
         <nav className={styles.nav}>
             <div className={styles.authLinks}>
                 {auth.user ? (
-                    <a href="/logout" className="user-link">
+                    <a href="/logout" onClick={handleLogout}>
                         logout
                     </a>
                 ) : (
-                    <Link to="/login" className="user-link">
-                        login
-                    </Link>
+                    <Link to="/login">login</Link>
                 )}
             </div>
         </nav>
