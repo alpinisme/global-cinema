5867b2f8fec8c768b10a72ef4748ae53f0ea7ed6
Matthew Nelson <menelso2@illinois.edu>
2021-03-26 20:49:52 -0500

    Fix annoying error logging in test
    The test intentionally throws an error
    So the tests pass, but the error was still
    logged to the console. So I spy on console.error
    now, which is the best solution I've found

--- resources/ts/tests/ErrorBoundary.test.tsx
+++ resources/ts/tests/ErrorBoundary.test.tsx
@@ -5,13 +5,17 @@ import '@testing-library/jest-dom/extend-expect';
 
 describe('ErrorBoundary', () => {
     it('should show the error but not fail if a child component throws an error', () => {
+        const spy = jest.spyOn(console, 'error');
+        spy.mockImplementation(() => undefined);
+
         const errMsg = 'Oops. I failed.';
         const msg = 'What I wish I could render';
 
         function Thrower() {
             throw new Error(errMsg);
-            return <div>msg</div>;
+            return <div>{msg}</div>;
         }
+
         render(
             <ErrorBoundary>
                 <Thrower />
@@ -21,5 +25,7 @@ describe('ErrorBoundary', () => {
         expect(screen.getByText('Something went wrong.')).toBeInTheDocument();
         expect(screen.getByText('Error: ' + errMsg)).toBeInTheDocument();
         expect(screen.queryByText(msg)).not.toBeInTheDocument();
+
+        spy.mockRestore();
     });
 });
