195d8ee86b5cc7bc1c42004675d61e4d530709d7
Matthew Nelson <menelso2@illinois.edu>
2021-08-22 16:50:23 -0500

    Clarify password in UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\User;
+use Hash;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -27,7 +28,7 @@ class UserFactory extends Factory
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
             'role' => $this->faker->randomElement(User::ROLES),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make("password"),
             'remember_token' => Str::random(10),
         ];
     }
