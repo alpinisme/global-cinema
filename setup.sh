9c0153e29c316b3caabfe586d57de2857f943fb0
Matthew Nelson <menelso2@illinois.edu>
2021-08-21 19:09:13 -0500

    Add setup file for quick dev setup on new machine

---
+++ setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# check for needed commands, notify and exit if any unavailable
+for COMMAND in php npm composer
+do
+    if ! command -v $COMMAND &> /dev/null
+    then
+        echo "** $COMMAND required but not found"
+        COMMAND_MISSING=1
+    fi
+done
+
+if [[ $COMMAND_MISSING -eq 1 ]]
+then 
+    echo "Please install missing dependencies listed above"
+    exit 2
+fi
+
+echo "- Creating .env file from .env.example"
+cp .env.example .env 
+
+echo "- Installing composer dependencies"
+composer install -q || exit 1
+
+echo "- Generating app key"
+php artisan key:generate -q
+
+echo "- Setting up sqlite database and running migrations"
+touch database/database.sqlite
+php artisan migrate -q || exit 1
+
+echo "- Installing npm dependencies"
+npm install --quiet 1> /dev/null || exit 1
+
+echo "- Building dev bundle with webpack"
+npm run dev 1> /dev/null || exit 1
+
+echo "\nProject built. Run `php artisan serve` to start a dev server"
