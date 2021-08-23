7e9460f854fa8eff5dcb999ebbdcbc605237eff2
Matthew Nelson <menelso2@illinois.edu>
2021-08-23 11:40:15 -0500

    Add in-progress note

--- resources/ts/pages/Home/Admin/CsvUploader.tsx
+++ resources/ts/pages/Home/Admin/CsvUploader.tsx
@@ -3,6 +3,7 @@ import React, { ReactElement } from 'react';
 const CsvUploader = (): ReactElement => {
     return (
         <form>
+            <p>(Feature in progress)</p>
             <input type="file" />
         </form>
     );
