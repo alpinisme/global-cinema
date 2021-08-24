a75c1ad188f524ce82b5128f0f7ef56a173f5150
Matthew Nelson <menelso2@illinois.edu>
2021-08-24 18:44:45 -0500

    Remove 'data' wrapping on resource

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Http\Resources\GradingResource;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 
@@ -28,5 +29,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        GradingResource::withoutWrapping();
     }
 }
